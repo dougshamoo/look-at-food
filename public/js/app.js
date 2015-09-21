ad029fd762a233e06da6ebcdf59b578afd95e8ab
Doug Shamoo <doug.shamoo@gmail.com>
2015-09-21 14:43:35 -0700

    fix get request to search for foods

--- public/js/app.js
+++ public/js/app.js
@@ -1,28 +1,30 @@
 var lookAtFood = angular.module('lookAtFood', []);
 
 var apiKey = '95Asywfj2p8t3kQFsGLGKk12vt6gpqW2Yh99GPye';
-var apiSearch = 'api.nal.usda.gov/ndb/search';
+var apiSearch = 'http://api.nal.usda.gov/ndb/search';
 
 lookAtFood.controller('foodController', ['$scope', '$http', function($scope, $http) {
 
   $scope.searchItem = '';
   $scope.foodReport = {};
-  $scope.possibleFoods = [];
+  $scope.possibleFoods = {};
+  $scope.foodId = '';
 
   $scope.searchFoods = function() {
     var params = [
       '?format=json',
       '&q=', $scope.searchItem,
       '&sort=r',
-      '&max=10',
+      '&max=20',
       '&api_key=', apiKey
     ].join('');
 
     console.log('searching for foods');
     $http.get(apiSearch + params)
       .then(function(response) {
-        $scope.possibleFoods = response.data;
+        $scope.possibleFoods = response.data.list.item;
         console.log($scope.possibleFoods);
+        console.log(typeof $scope.possibleFoods);
       }, function(response) {
         console.log('error', response);
       });
