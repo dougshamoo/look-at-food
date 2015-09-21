be4b89db2fdb950302905d75571fa1dfc29cf184
Doug Shamoo <doug.shamoo@gmail.com>
2015-09-21 14:17:08 -0700

    implement buggy api call

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,30 @@
 var lookAtFood = angular.module('lookAtFood', []);
 
-lookAtFood.controller('foodController', ['$scope', '$http' function($scope, $http) {
+var apiKey = '95Asywfj2p8t3kQFsGLGKk12vt6gpqW2Yh99GPye';
+var apiSearch = 'api.nal.usda.gov/ndb/search';
+
+lookAtFood.controller('foodController', ['$scope', '$http', function($scope, $http) {
 
   $scope.searchItem = '';
+  $scope.foodReport = {};
+  $scope.possibleFoods = [];
 
-  
+  $scope.searchFoods = function() {
+    var params = [
+      '?format=json',
+      '&q=', $scope.searchItem,
+      '&sort=r',
+      '&max=10',
+      '&api_key=', apiKey
+    ].join('');
 
+    console.log('searching for foods');
+    $http.get(apiSearch + params)
+      .then(function(response) {
+        $scope.possibleFoods = response.data;
+        console.log($scope.possibleFoods);
+      }, function(response) {
+        console.log('error', response);
+      });
+  }
 }]);
