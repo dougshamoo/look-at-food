502707f39f968061ccf1db2d0214408c2ff0acfc
Doug Shamoo <doug.shamoo@gmail.com>
2015-09-21 16:05:10 -0700

    implemnt getFoodByID

--- public/js/app.js
+++ public/js/app.js
@@ -2,13 +2,13 @@ var lookAtFood = angular.module('lookAtFood', []);
 
 var apiKey = '95Asywfj2p8t3kQFsGLGKk12vt6gpqW2Yh99GPye';
 var apiSearch = 'http://api.nal.usda.gov/ndb/search';
+var apiReports = 'http://api.nal.usda.gov/ndb/reports'
 
 lookAtFood.controller('foodController', ['$scope', '$http', function($scope, $http) {
 
   $scope.searchItem = '';
   $scope.foodReport = {};
   $scope.possibleFoods = {};
-  $scope.foodId = '';
 
   $scope.searchFoods = function() {
     var params = [
@@ -19,14 +19,32 @@ lookAtFood.controller('foodController', ['$scope', '$http', function($scope, $ht
       '&api_key=', apiKey
     ].join('');
 
-    console.log('searching for foods');
+
+    console.log('searching for foods...');
     $http.get(apiSearch + params)
-      .then(function(response) {
-        $scope.possibleFoods = response.data.list.item;
-        console.log($scope.possibleFoods);
-        console.log(typeof $scope.possibleFoods);
-      }, function(response) {
-        console.log('error', response);
+      .then(function(res) {
+        $scope.possibleFoods = res.data.list.item;
+        // console.log($scope.possibleFoods);
+        // console.log(typeof $scope.possibleFoods);
+      }, function(res) {
+        console.log('error', res);
       });
   }
+
+  $scope.getFoodByID = function(id) {
+    var params = [
+      '?format=json',
+      '&ndbno=', id,
+      '&type=b',
+      '&api_key=', apiKey
+    ].join('');
+    $http.get(apiReports + params)
+      .then(function(res) {
+        $scope.foodReport = res.data
+      }, function(res) {
+        console.log('ERROR:', res)
+      });
+
+  } 
+
 }]);
